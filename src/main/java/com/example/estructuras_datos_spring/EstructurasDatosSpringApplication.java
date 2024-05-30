7223cf50f3346300e87b8d639f7c40a831685bcd
programador7 <programador7@tecnologicocomfenalco.edu.co>
2024-05-30 09:28:31 -0500

    estructuras de datos en java

---
+++ src/main/java/com/example/estructuras_datos_spring/EstructurasDatosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.estructuras_datos_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstructurasDatosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstructurasDatosSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/estructuras_datos_spring/EstructurasDatosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estructuras_datos_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstructurasDatosSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
